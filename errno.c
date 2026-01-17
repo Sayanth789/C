d4296ecf93287736ec2dc327bdf9f973cc9ff3d3
Sayanth <sayanthstrng@gmail.com>
2026-01-17 15:17:20 +0530

    Uploading the errno

---
+++ errno.c
@@ -0,0 +1,52 @@
+// Check for whether  the system uses errno for the math errors.
+// perform several math opetations and after each operatios it checks whether an error occured.
+// Prints human redable message describing that error.
+
+
+#include <stdio.h>
+#include <errno.h>
+#include <math.h>
+
+void show_errno(void) {
+    const char *err_info = "unknown error";
+    switch (errno) {
+    case EDOM:
+        err_info = "domain error";
+        break;
+    case EILSEQ:
+        err_info = "illegal sequence";
+        
+        break;
+    case ERANGE:
+        err_info = "pole or range error";
+        break;
+    case 0:
+        err_info = "no error";    
+
+    }
+    fputs(err_info, stdout);
+    puts(" occured");
+}
+
+int main(void) { 
+    fputs("MATH_ERRNO is ", stdout);
+    puts(math_errhandling & MATH_ERRNO ? "set" : "not set");
+
+
+    errno = 0;
+    1.0/0.0;
+    show_errno();
+
+    errno = 0;
+    acos(+1.1);
+    show_errno();
+
+    errno = 0;
+    log(0.0);
+    show_errno();
+
+    errno = 0;
+    sin(0.0);
+    show_errno();
+
+} 
