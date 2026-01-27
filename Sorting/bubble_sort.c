b4952dfc645513640c32d7c6b52681822dc94c0d
Sayanth <sayanthstrng@gmail.com>
2026-01-27 14:31:34 +0530

    uploading bubble sort

---
+++ Sorting/bubble_sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// swap elements 
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// Implements BubbleSort 
+
+void bubbleSort(int arr[], int n) {
+    int i, j;
+
+    int swapping = 0;
+    for (i = 0; i < n-1; i++) {
+            for (j=0; j<n-i-1; j++) {
+            // last i elements are already in place.
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+                swapping = 1;
+            }
+        }
+        if (swapping == 0) // if swapping is 0, that means the array is sorted so no need to go for next.
+            break;
+    }
+}
+
+// Function to print elements 
+void printArray(int arr[], int size) {
+    int i;
+    for (i=0; i<size; i++) {
+        printf("%d", arr[i]);
+        printf("\n");
+    }
+}
+
+// Driver code 
+int main() {
+    int arr[] = {46, 24, 33, 10, 2, 81, 50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printf("Sorted Array: \n");
+    printArray(arr, n);
+    return 0;
+}
