81a3f5819f00f226de23247590a2b1a002d0d137
Sayanth <sayanthstrng@gmail.com>
2026-03-17 17:51:41 +0530

    yeah sucks

---
+++ guessing_game.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    int secretNumber = 5;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outOfGuesses = 0;
+
+    while (guess != secretNumber && outOfGuesses == 0) {
+
+        if (guessCount < guessLimit) {
+
+            printf("Enter a numner: ");
+            scanf("%d", &guess);
+            guessCount++;
+
+        } else {
+            outOfGuesses == 1;
+        }
+       
+    }
+    if (outOfGuesses == 1) {
+        printf("Out of Guesses..");
+    }
+    else {
+    printf("Yeah Correct Guess...!");
+    }
+
+   
+}
