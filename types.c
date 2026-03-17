68da33c8a668a0381f7c7d16331ec823c5d9aa4b
Sayanth <sayanthstrng@gmail.com>
2026-03-17 17:50:17 +0530

    some types

---
+++ types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int age = 40;
+    double gpa = 4.3;
+    char grade = 'A';
+    char phrase[] = "This is a beautiful day...!";
+
+    
+
+
+
+    return 0;
+}
