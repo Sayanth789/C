0230c192d6c063de6233b044f72e62c116d15363
Sayanth <sayanthstrng@gmail.com>
2026-03-24 09:05:04 +0530

    oop in c

---
+++ oop.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct Point {
+    double x;
+    double y;
+
+    void (*del)(struct Point* self);
+    void (*dump)(const struct Point *self);
+    double (*origin)(const struct Point* self);
+};
+
+void point_dump(const struct Point* self) {
+    printf("Object point@%p x=%f y=%f\n",
+           self, self->x, self->y);
+}
+
+void point_del(struct Point* self) {
+    free(self);
+}
+
+double point_origin(const struct Point* self) {
+    return sqrt(self->x*self->x + self->y*self->y);
+}
+
+struct Point *point_new(double x, double y) {
+    struct Point *p = malloc(sizeof(*p));
+
+    if (p == NULL)
+        return NULL;
+
+    p->x = x;
+    p->y = y;
+    p->dump = point_dump;
+    p->origin = point_origin;
+    p->del = point_del;
+
+    return p;
+}
+
+int main(void) {
+    struct Point *pt = point_new(4.0, 5.0);
+
+    pt->dump(pt);
+    printf("Origin %f\n", pt->origin(pt));
+
+    pt->del(pt);
+}
