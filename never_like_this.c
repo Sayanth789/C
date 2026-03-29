1865465e88fcc82704e3caa22b30a42ff15c6bb2
Sayanth <sayanthstrng@gmail.com>
2026-03-29 07:40:47 +0530

    Update never_like_this.c

--- never_like_this.c
+++ never_like_this.c
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <cassert>
 
+// Bug: t = max(x, y). If y > x, the else branch sets t = y,
+// making t > x true and triggering assert(false).
+// Thus the else case always fails even though it is valid.
+
+
 void foo(int x, int y) {
     int t = 0;
 
