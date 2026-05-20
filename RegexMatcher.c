14f041d29fad0851b50e626d3cfea721228f17f2
Sayanth <sayanthstrng@gmail.com>
2026-05-20 22:25:49 +0530

    regex matcher ans

---
+++ RegexMatcher.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isMatch(char *s, char *p) {
+    int m = strlen(s);
+    int n = strlen(p);
+
+    bool dp[m + 1][n + 1];
+    memset(dp, 0, sizeof(dp));
+
+    dp[0][0] = true;
+
+    // Handle patterns like a*, a*b*, a*b*c* matching empty string
+    for (int j = 2; j <= n; j++) {
+        if (p[j - 1] == '*') {
+            dp[0][j] = dp[0][j - 2];
+        }
+    }
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+
+            if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else if (p[j - 1] == '*') {
+
+                // Case 1: ignore x*
+                dp[i][j] = dp[i][j - 2];
+
+                // Case 2: use x* if it matches
+                if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+int main() {
+    char s1[] = "aab";
+    char p1[] = "c.a*b";
+
+    if (isMatch(s1, p1)) {
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
+
+    return 0;
+}
