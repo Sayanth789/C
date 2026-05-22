a44512c80d3047e2acc96022c536044301991ad3
Sayanth <sayanthstrng@gmail.com>
2026-05-22 09:58:42 +0530

    Update 1.c

--- pointers/basics/1.c
+++ pointers/basics/1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int x = 10;
+    int *p = &x;
+
+    printf("%d\n", x);
+    printf("%p\n", &x),
+    printf("%p\n", p);
+    printf("%d\n", *p);
+
+
+    return 0;
+}
