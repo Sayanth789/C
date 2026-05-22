a5e546af2d96571070df373707ec5e2911a10fc7
Sayanth <sayanthstrng@gmail.com>
2026-05-22 20:35:44 +0530

    compare as used in sort?

---
+++ fun_ptr2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* left, const void* right) {
+    return (*(int*)right - *(int *)left);
+    
+}
+
+void main() {
+    int (*cmp) (const void*, const void*);
+    cmp= compare;
+
+    int iarray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    qsort(iarray, sizeof(iarray)/sizeof(*iarray),sizeof(*iarray), cmp);
+
+    int c = 0;
+    while (c < sizeof(iarray)/sizeof(*iarray)) {
+        printf("%d\n", iarray[c]);
+        c++;
+    } 
+
+}
