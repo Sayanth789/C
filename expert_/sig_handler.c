517e49b0da9066523ccd50cac307c242b0c0a1da
Sayanth <sayanthstrng@gmail.com>
2026-05-29 08:43:03 +0530

    Add files via upload

---
+++ expert_/sig_handler.c
@@ -0,0 +1,23 @@
+// A signal handler to catch the segv Signal
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void handler(int s) {
+    if (s == SIGBUS) printf(" now got a bus error signal\n");
+    if (s == SIGSEGV) printf("now got a segmentation violation signal\n");
+    if (s == SIGILL) printf("Now got an illegal instruction signal\n");
+
+    exit(1);
+}
+
+int main() {
+    int *p = NULL;
+    signal(SIGBUS, handler);
+    signal(SIGSEGV, handler);
+    signal(SIGILL, handler);
+    *p = 0;
+
+}
