0dfa48ccbb3856c1f2762aad6936558a06ce15bf
Sayanth <sayanthstrng@gmail.com>
2026-06-19 17:18:45 +0530

    Add wh_question.c with basic output logic need refactor

---
+++ wh_question.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int output;
+    if (output == 1) {
+        printf("Who");
+    }
+    else if (output == 2) {
+        printf("What");
+    }
+    ...
+}
